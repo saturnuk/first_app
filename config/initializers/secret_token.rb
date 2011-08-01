5bc36a62029f1cfc8172d8f14fce61d386ce070e
euts <euts@euts.(none)>
2011-08-01 18:47:58 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '01833d604d28c84885d1c9036fd7712c5071cf7964d3ec36d2172e5c32f2babd10f80318b34f4c3858b6d4c78877f6cf73360ac0a9482990e30aae2407be7342'
